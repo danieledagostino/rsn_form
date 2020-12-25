f9893f05d1d568a353de0a4540412834f1ea7540
Daniele D'Agostino <principelele@gmail.com>
2020-12-25 23:16:32 +0100

    DateField widget added

---
+++ rsn_form/lib/form_widget/date_field.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:date_field/date_field.dart';
+import 'package:rsn_form/rsn_stepper.dart';
+
+class DateField {
+  final TextEditingController controller = TextEditingController();
+  final String question;
+  DateTime selectedDate;
+  BuildContext context;
+
+  DateField({Key key, @required this.context, @required this.question});
+
+  List<Widget> getWidgets() {
+    return <Widget>[
+      Text(question),
+      DateTimeField(
+        selectedDate: selectedDate,
+        onDateSelected: (DateTime date) =>
+            RsnStepper.of(context).setState(() => selectedDate = date),
+        lastDate: DateTime(2020),
+      ),
+    ];
+  }
+}
