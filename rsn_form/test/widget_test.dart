a88e5152029ddb20bebbb299eaba53ee85861989
Daniele D'Agostino <principelele@gmail.com>
2020-12-26 12:03:44 +0100

    unit test

--- rsn_form/test/widget_test.dart
+++ rsn_form/test/widget_test.dart
@@ -1,30 +1,21 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility that Flutter provides. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:rsn_form/main.dart';
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:rsn_form/utility/make_step.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+  test('make widgets from json configuration', () {
+    final file = new File('./test_resources/widget_conf.json');
+    Future<String> decodedJson = file.readAsString();
+    decodedJson.then((value) {
+      final restJson = json.decode(value);
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+      MakeStep makeStep = MakeStep.test(restJson);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+      List<Step> steps = makeStep.steps(null);
+    });
   });
 }
